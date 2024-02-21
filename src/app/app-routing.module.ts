d803b4497e5f26ad9c7f9e87346c55a2ba491ded
Mehakdeep Singh Dhaliwal <dhalimeh@sheridancollege.ca>
2024-02-21 13:58:23 -0500

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
 
-const routes: Routes = [];
+import { Routes, RouterModule } from '@angular/router';
+import { InputComponent } from './input/input.component';
+import { OutputComponent } from './output/output.component';
+
+const routes: Routes = [
+  { path: 'input', component: InputComponent },
+  { path: 'output', component: OutputComponent },
+  { path: '', redirectTo: '/input', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/data.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+export interface EmployeeData {
+  name: string;
+  status: string;
+  certified: boolean;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  private employeeData = new BehaviorSubject<EmployeeData | null>(null);
+  currentEmployeeData = this.employeeData.asObservable();
+
+  constructor() {}
+
+  updateEmployeeData(data: EmployeeData) {
+    this.employeeData.next(data);
+  }
+}
